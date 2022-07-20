f5c317465999e1209bc8460af1475166bbe520ae
AgnidDrage <marianosancheztoledo@gmail.com>
2022-07-20 18:00:26 -0300

    First Commit

---
+++ backend/src/main/java/com/franquicia/backend/producto/Producto.java
@@ -0,0 +1,24 @@
+package com.franquicia.backend.producto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+}
