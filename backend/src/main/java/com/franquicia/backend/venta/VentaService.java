6b3be2a9786c490308dd8df33ef329dcc7244d57
AgnidDrage <marianosancheztoledo@gmail.com>
2022-08-12 12:02:13 -0300

    Added Ventas

---
+++ backend/src/main/java/com/franquicia/backend/venta/VentaService.java
@@ -0,0 +1,28 @@
+package com.franquicia.backend.venta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VentaService {
+    private final VentaRepository ventaRepository;
+
+    @Autowired
+    public VentaService(VentaRepository ventaRepository) {
+        this.ventaRepository = ventaRepository;
+    }
+
+    public List<Venta> ventaList() { return ventaRepository.findAll(); }
+
+    public Optional<Venta> ventaById(Long id) { return ventaRepository.findById(id); }
+
+    public HttpStatus addVenta(Venta venta) {
+        ventaRepository.save(venta);
+        return HttpStatus.CREATED;
+    }
+
+}
